70ce383828377abcbe14248b792340286fe12c49
Mohammed Abdur Rahmaan <112614695+abdurrahmaan11265@users.noreply.github.com>
2024-12-12 00:22:52 +0530

    Enhance JournalPage functionality and UI
    - Integrated search parameters to fetch journal entries based on student ID.
    - Updated the fetchEntries function to conditionally retrieve entries for students and counselors.
    - Improved UI elements, including updated headings and added notice for confidentiality in new entry section.
    - Enhanced emotional statistics display with dynamic data from journal entries.
    - Refactored entry handling logic for better clarity and maintainability.
    
    These changes improve the user experience and functionality of the journaling feature, ensuring better data handling and presentation.

--- Front_End/my-educational-platform/src/pages/JournalPage.js
+++ Front_End/my-educational-platform/src/pages/JournalPage.js
@@ -5,6 +5,7 @@ import { MdClose } from 'react-icons/md';
 import { MdOutlineModeEditOutline } from "react-icons/md";
 import { MdDeleteSweep } from "react-icons/md";
 import { LuSaveAll } from "react-icons/lu";
+import { useSearchParams } from 'react-router-dom';
 import {
   getJournalEntries,
   addJournalEntry,
@@ -24,19 +25,15 @@ const JournalPage = () => {
   const [newEntry, setNewEntry] = useState('');
   const [isUpdating, setIsUpdating] = useState(false);
   const [entryToUpdate, setEntryToUpdate] = useState(null);
-
-  const xAxisData = ['January', 'February', 'March', 'April', 'May', 'June'];
-  const yAxisData = [30, 45, 28, 60, 80, 95];
-  const xAxisData1 = ['Red', 'Green']; // X-axis labels for the two bars
-  const yAxisData1 = [65, 15];
+  const [searchParams] = useSearchParams();
+  const studentId = searchParams.get('studentId');
 
   const toggleEntryClass = (e) => {
     const entryElement = e.currentTarget.closest('.journal-entry');
-  
+
     if (entryElement) {
       entryElement.classList.add('journal-entry-on-click');
       const statsSections = document.body.querySelectorAll('.journal-analytics');
-      console.log(statsSections);
       if (statsSections) {
         statsSections.forEach((statsSection) => {
           statsSection.style.display = 'flex';
@@ -59,16 +56,22 @@ const JournalPage = () => {
   };
 
   useEffect(() => {
-    const fetchEntries = async () => {
+    const fetchEntries = async (studentId) => {
       try {
-        const data = await getJournalEntries(userData._id, authToken);
+        const data = await getJournalEntries(studentId, authToken);
         setEntries(data);
       } catch (error) {
         console.error("Error fetching journal entries:", error);
       }
     };
-    fetchEntries();
-  }, [userData._id, authToken]);
+    if (userData.userType === 'student') {
+      fetchEntries(userData._id);
+    }
+    else {
+      fetchEntries(studentId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleAddEntry = async () => {
     if (newEntry.trim() !== '') {
@@ -76,17 +79,15 @@ const JournalPage = () => {
         const response = await addJournalEntry(userData._id, newEntry, authToken);
         setNewEntry('');
         setEntries(response.journalEntries);
-      } catch (error){
+      } catch (error) {
         console.error("Error adding journal entry:", error);
       }
     }
   };
 
   const handleUpdateButton = (entryId, currentText, e) => {
-   
     setIsUpdating(true);
     setEntryToUpdate({ id: entryId, text: currentText });
-    // closeEntry(e);
   };
 
   const handleUpdateEntry = async () => {
@@ -111,12 +112,22 @@ const JournalPage = () => {
     }
   };
 
+  const getEmotionsData = (entry) => {
+    return Object.values(entry.emotions);
+  };
+
+  const getPositivityNegativityData = (entry) => {
+    const positivity = entry.positivity;
+    const negativity = 100 - positivity;
+    return [positivity, negativity];
+  };
+
   return (
     <div className="journal-page">
       <HeaderStudent header_name={"Journal"} />
       <main className="journal-content">
         <aside className="journal-entries">
-          <h2>Previous Entries</h2>
+          <h2>Journal Entries</h2>
           <div className="entries-list">
             {entries.map((entry) => (
               <div key={entry._id} className="journal-entry" onClick={toggleEntryClass}>
@@ -128,57 +139,61 @@ const JournalPage = () => {
                 </div>
                 <div className='two-section-of-journal'>
                   <div className='entry-text-and-button-container'>
-                  <div className="entry-text">
-                    {isUpdating && entryToUpdate?.id === entry._id ? (
-                      <textarea rows={10}
-                        value={entryToUpdate.text}
-                        onChange={(e) => setEntryToUpdate({ ...entryToUpdate, text: e.target.value })}
-                      />
-                    ) : (
-                      entry.note
+                    <div className="entry-text">
+                      {isUpdating && entryToUpdate?.id === entry._id ? (
+                        <textarea rows={10}
+                          value={entryToUpdate.text}
+                          onChange={(e) => setEntryToUpdate({ ...entryToUpdate, text: e.target.value })}
+                        />
+                      ) : (
+                        entry.note
+                      )}
+                    </div>
+                    {userData.userType === 'student' && (
+                      <div className='buttons-journal-entries'>
+                        {isUpdating && entryToUpdate?.id === entry._id ? (
+                          <button className='save-button-journal' onClick={() => {
+                            handleUpdateEntry();
+                          }}><LuSaveAll /></button>
+                        ) : (
+                          <button className='edit-button-journal' onClick={(e) => handleUpdateButton(entry._id, entry.note, e)}><MdOutlineModeEditOutline /></button>
+                        )}
+                        <button className='delete-button-journal' onClick={(e) => {
+                          handleDeleteEntry(entry._id);
+                          closeEntry(e);
+                        }}><MdDeleteSweep /></button>
+                      </div>
                     )}
                   </div>
-                  <div className='buttons-journal-entries'>
-                  {isUpdating && entryToUpdate?.id === entry._id ? (
-                    <button className='save-button-journal' onClick={() => {
-                      handleUpdateEntry();
-                    }}><LuSaveAll /></button>
-                  ) : (
-                    
-                    <button className='edit-button-journal' onClick={(e) => handleUpdateButton(entry._id, entry.note, e)}><MdOutlineModeEditOutline /></button>
-                  )}
-                  <button className='delete-button-journal' onClick={(e) => {
-                    handleDeleteEntry(entry._id);
-                    closeEntry(e);
-                  }}><MdDeleteSweep /></button>
-                  </div>
-                  </div>
-                  <div className='journal-analytics' style={{textAlign:'center'}}>
+                  <div className='journal-analytics' style={{ textAlign: 'center' }}>
                     <h2>Emotional Statistics</h2>
-                    <CurveChart xData={xAxisData} yData={yAxisData} />
-                    <BarChart xData={xAxisData1} yData={yAxisData1} />
+                    <CurveChart xData={Object.keys(entry.emotions)} yData={getEmotionsData(entry)} />
+                    <BarChart xData={['Positivity', 'Negativity']} yData={getPositivityNegativityData(entry)} />
                     <div className='Theme_container'>
-                    <h4 style={{marginBottom:'10px'}}>Theme:</h4>
-                    <p>A journal is a sacred space where thoughts, emotions, and experiences are poured onto the page, creating a tangible record of one's inner world.</p>
+                      <h4 style={{ marginBottom: '10px' }}>Theme:</h4>
+                      <p>{entry.theme}</p>
                     </div>
-
                   </div>
                 </div>
               </div>
-              
             ))}
           </div>
         </aside>
-        <section className="new-entry-section">
-          <h2>New Entry</h2>
-          <textarea
-            rows={10}
-            value={newEntry}
-            onChange={(e) => setNewEntry(e.target.value)}
-            placeholder="Write your new journal entry here..."
-          />
-          {isUpdating ? <button onClick={handleUpdateEntry}>Update</button> : <button onClick={handleAddEntry}>Add New Entry</button>}
-        </section>
+        {userData.userType === 'student' && (
+          <section className="new-entry-section">
+            <h2>New Entry</h2>
+            <div className="notice">
+              NOTE: These entries are confidential and are only visible to your counsellor.
+            </div>
+            <textarea
+              rows={10}
+              value={newEntry}
+              onChange={(e) => setNewEntry(e.target.value)}
+              placeholder="Write your new journal entry here..."
+            />
+            {isUpdating ? <button onClick={handleUpdateEntry}>Update</button> : <button onClick={handleAddEntry}>Add New Entry</button>}
+          </section>
+        )}
       </main>
     </div>
   );
