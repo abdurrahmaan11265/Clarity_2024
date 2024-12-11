c3c41a47850bcce8a8b820840039bf25de831377
Mohammed Abdur Rahmaan <112614695+abdurrahmaan11265@users.noreply.github.com>
2024-12-11 10:51:24 +0530

    Update API service base URL for production deployment
    - Changed the base URL in api.js from localhost to the production URL (https://clarity-2024.onrender.com).
    - Commented out the local development URL for clarity.
    
    This update ensures that the application connects to the correct API endpoint in the production environment.

--- Front_End/my-educational-platform/src/services/api.js
+++ Front_End/my-educational-platform/src/services/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5523',
-    // baseURL: 'https://clarity-2024.onrender.com',
+    // baseURL: 'http://localhost:5523',
+    baseURL: 'https://clarity-2024.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
